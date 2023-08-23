96eb95495321a614e77c07e5e8d70c933bf6c9e9
Shawn Ning <shawnningjiale@gmail.com>
2023-08-23 10:39:41 +0800

    upppp

--- routes/tvshow.js
+++ routes/tvshow.js
@@ -2,31 +2,65 @@ const express = require("express");
 const router = express.Router();
 
 // import model into router
-const tvshow = require("../models/tvshow");
+const Tvshow = require("../models/tvshow");
 
-/* list all the movies */
+/* list all the tvshows */
 router.get("/", async (req, res) => {
-  const { genre, rating, premiere_year } = req.query;
+  const { premiere_year, genre, rating } = req.query;
   let filter = {};
-  /* better filtering method */
-  if ([genre] || rating || premiere_year) {
+  if (genre || rating || premiere_year) {
     if (genre) {
-      filter.genre = genre; // { genre: genre }
+      filter.genre = { $in: genre.split(",") }; // { genre: { $in: genre } }
     }
     if (rating) {
       filter.rating = { $gt: rating }; // { rating: { $gt: rating } }
     }
     if (premiere_year) {
-      filter.premiere_year = { $gt: premiere_year }; // { release_year: { $gt: release_year } }
+      filter.premiere_year = { $gt: premiere_year }; // { premiere_year: { $gt: release_year } }
     }
   }
-
-  res.send(await tvshow.find(filter));
+  const list = await Tvshow.find(filter);
+  res.send(list);
 });
 
+/* get specific tvshow by id */
 router.get("/:id", async (req, res) => {
-  const onetvshow = await tvshow.findOne({ _id: req.params.id });
-  res.send(onetvshow);
+  const data = await Tvshow.findOne({ _id: req.params.id });
+  res.send(data);
+});
+
+router.post("/", async (req, res) => {
+  // create a placeholder for a new movie
+  const newTvshow = new Tvshow({
+    title: req.body.title,
+    creator: req.body.creator,
+    premiere_year: req.body.premiere_year,
+    end_year: req.body.end_year,
+    seasons: req.body.seasons,
+    genre: req.body.genre,
+    rating: req.body.rating,
+  });
+  // save the movie into mongodb
+  await newTvshow.save();
+  res.send(newTvshow);
+});
+
+router.put("/:id", async (req, res) => {
+  // get movie id
+  const tvshow_id = req.params.id;
+  // update the movie
+  const updatedTvshow = await Tvshow.findByIdAndUpdate(tvshow_id, req.body, {
+    // new: true, // return the modified data
+  });
+  res.send(updatedTvshow);
+});
+
+router.delete("/:id", async (req, res) => {
+  // get movie id
+  const tvshow_id = req.params.id;
+  // delete the movie
+  const deletedTvshow = await Tvshow.findByIdAndDelete(tvshow_id);
+  res.send(deletedTvshow);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+// create an instance of express
 const app = express();
 
+// middleware to handle JSON request
+app.use(express.json());
+
+// MongoDB connection
 mongoose
   .connect("mongodb://127.0.0.1:27017/netflix")
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-//routes
+// routes
 const movieRouter = require("./routes/movie");
 const tvshowRouter = require("./routes/tvshow");
 
@@ -16,9 +21,10 @@ app.use("/movies", movieRouter);
 app.use("/tvshows", tvshowRouter);
 
 app.get("/", (req, res) => {
-  res.send("<a href='/movies'>Movies</a> <a href='/tvshows'>TVShows</a>");
+  res.send("<a href='/movies'>Movies</a>");
 });
 
+// start the server
 app.listen(5000, () => {
   console.log("Server is running at http://localhost:5000");
 });
